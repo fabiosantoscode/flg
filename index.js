eec23a8db9fc3be667fbac62e5f2ec0efb29fc15
Fábio Santos <fabiosantosart@gmail.com>
2019-09-21 18:12:10 +0100

    small improvements

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var hop = {}.hasOwnProperty
+
 module.exports = function (s, defaults) {
   s = s || ''
   var tags = s.split(/(\s+|(?=[+-]))/)
@@ -9,7 +11,9 @@ module.exports = function (s, defaults) {
   })
   return tags.reduce(function (accum, tag) {
     tag = tag.trim()
-    if (tag) accum[tag.replace(/^[+-]/, '')] = tag[0] !== '-'
+    if (tag) {
+      accum[tag.replace(/^[+-]/, '')] = tag[0] !== '-'
+    }
     return accum
   }, out)
 }
@@ -18,17 +22,17 @@ module.exports.stringify = function (object, defaults, options) {
   options = options || {}
   var out = ''
   var keys = Object.keys(object).sort()
-  keys.forEach(function (key) {
+  keys.forEach(function (key, i) {
     var value = !!object[key]
-    var hasDefault = defaults && (key in defaults)
+    var hasDefault = defaults && hop.call(defaults, key)
     var defaultValue = defaults && !!defaults[key]
     if (defaults && hasDefault && value === defaultValue) {
       return
     }
-    if (options.spaces !== false) out += ' '
+    if (options.spaces !== false && i > 0) out += ' '
     out += value === true
       ? '+' + key
       : '-' + key
   })
-  return out.substring(1) // trim starting space
+  return out
 }
